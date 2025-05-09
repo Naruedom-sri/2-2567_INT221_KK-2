12dfee15a4766f8659688ef95699f0c164d3da33
noppawut phomchana <142074952+saigrogyh@users.noreply.github.com>
2025-05-09 23:04:17 +0700

    114[update]Update to meet requirement

--- backend/src/main/java/intregatedproject/backend/dtos/RequestSaleItemDto.java
+++ backend/src/main/java/intregatedproject/backend/dtos/RequestSaleItemDto.java
@@ -11,12 +11,26 @@ import java.math.BigDecimal;
 public class RequestSaleItemDto {
     private Integer id;
     private String model;
+    public void setModel(){
+        this.model = model.trim();
+    }
     private Integer ramGb;
     private Integer storageGb;
     private int price;
     private ResponseBrandDto brand;
     private String description;
+    public void setDescription(){
+        this.description = description.trim();
+    }
     private BigDecimal screenSizeInch;
     private String color;
+    public void setColor(){
+        this.color = color.trim();
+    }
     private Integer quantity;
+    public void setQuantity(){
+        if(this.quantity == null || this.quantity < 0){
+            this.quantity = 1;
+        }
+    }
 }
